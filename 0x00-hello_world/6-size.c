ea8b2e5260c2f6ef3da0549a645106aab2c085d2
roua <rouabadraoui158@gmail.com>
2023-08-13 17:10:20 -0700

    printf siseof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - a program that prints the size of various types on computer
- * Return 0:(success)
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char: %lu bytes(s)\n",(unsigned long )sizeof(a));
-printf("size of an int: %lu bytes(s)\n",(unsigned long )sizeof(b));
-printf("size of a long int: %lu bytes(s)\n",(unsigned long )sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n",(unsigned long )sizeof(d));
-printf("size of a float: %lu bytes(s)\n",(unsigned long )sizeof(f));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
