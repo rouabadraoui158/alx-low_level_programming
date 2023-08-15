e92d22bf6572b73b8a7133784a75cd7020fec9ad
roua <rouabadraoui158@gmail.com>
2023-08-15 15:15:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+        char letter= 'a';
+	if (letter <= 'z')
+	{
+	  putchar(letter);
+          letter++;
+	} 
+	putchar  ('\n');
+	return (0);
+}
