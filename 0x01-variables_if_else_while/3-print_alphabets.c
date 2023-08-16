60b8ea87651817092825422e3516da0ae2a5e46c
roua <rouabadraoui158@gmail.com>
2023-08-16 12:57:24 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-char ch = 'a';
-while (ch <= 'z')
-{
-	if (ch != 'e' && ch != 'q')
+	char ch = 'a';
+
+	while (ch <= 'z')
 	{
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
-	ch++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
