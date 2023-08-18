ca4499b69543751aaa475a2c225ac703503e7c54
roua <rouabadraoui158@gmail.com>
2023-08-18 12:27:06 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-# include "main.h"
-/*
- * main - programming print _putchar
- * followed by newline
- * Return 0 (success)
- */
-int main(void)
-{
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,4 +18,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+	}
