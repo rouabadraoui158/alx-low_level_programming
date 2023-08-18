af66c3ccd251f8e90d7aceab1874e6b217585af9
roua <rouabadraoui158@gmail.com>
2023-08-18 14:16:38 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * main - a function that checks for alphabetic character.
+ * Returns 1 if c is a letter, lowercase or uppercase
+ * Returns 0 otherwise
+ */
+int main(void)
+{
+	_isalpha();
+}
