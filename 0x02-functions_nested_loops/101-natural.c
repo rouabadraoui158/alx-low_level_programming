a4d26d231892857fb7dc956e8235124e3d717502
roua <rouabadraoui158@gmail.com>
2023-08-21 10:15:53 -0700

    multuple numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/*
+ *main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int n, sum;
+
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
