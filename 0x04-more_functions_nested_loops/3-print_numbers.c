df392fab83251c6012eea4c75152e301ad8d80dd
roua <rouabadraoui158@gmail.com>
2023-08-22 03:17:56 -0700

    print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
+
+/**
+ *print_numbers - writes the character c to stdout
+ *@c: The character to print
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ */
 void print_numbers(void)
 {
-    int i;
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(i + '0');
-    }
-    _putchar('\n'); 
+	int i;
+
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
