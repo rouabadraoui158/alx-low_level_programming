440673fc18b649f011276ad0ab7fbb0a539b022f
roua <rouabadraoui158@gmail.com>
2023-08-22 05:30:24 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,18 @@
  */
 void more_numbers(void)
 {
-	int i,k;
-
-
-        for (i = 0; i < 9; i++)
-        {
-                for (k = 0; k < 15; k++)
-                {
+	int i, k;
+	
+	for (i = 0; i < 9; i++)
+	{
+		for (k = 0; k < 15; k++)
+		{
 			if (k > 9)
 			{
 				_putchar(k / 10 + '0');
 			}
 			_putchar(k % 10 + '0');
-                }
+		}
 		_putchar('\n');
-        }
-     
+	}
 }
