05252485edf549cc0101953662c4da658c2229dc
roua <rouabadraoui158@gmail.com>
2023-08-23 03:17:29 -0700

    zero_positive

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * positive_or_negative - checks signs of i
+ * @i : integer to check
  *
  * Return: Always 0 (Success)
  */
-int positive_or_negative(int i){
-if (i > 0)
+int positive_or_negative(int i)
 {
-printf("%d is positive\n", i);
-}
-if (i < 0)
-{
-printf("%d is negative\n", i);
-}
-if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-return (0);
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	return (0);
 }
