1d5b8c72ad1d1ef0141ef62db428853d500abafe
roua <rouabadraoui158@gmail.com>
2023-08-24 07:11:48 -0700

    reset

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -13,5 +13,6 @@ int main(void)
     n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
+    printf("n=%d\n", n);
     return (0);
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,10 +8,5 @@
  */
 void reset_to_98(int *n)
 {
-	int var = 98;
-	
-
-	n = &var;
-	printf("n =%d\n", *n);
-
+*n = 98;
 }
