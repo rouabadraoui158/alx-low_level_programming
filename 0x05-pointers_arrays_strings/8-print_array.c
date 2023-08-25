52a2680b3067d0c70098363c9747096345b651e0
roua <rouabadraoui158@gmail.com>
2023-08-25 13:32:47 -0700

    zero_positive

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_array - prints n elements of an array of integers
- *followed by new line
- *@n: is the number of elements of the array to be printed
- *
- * Return: Always 0.
- */
+*print_array - print n element of array
+*@a: array
+*@n: number of elements
+*Return: nothing
+*/
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
+int inc;
+for (inc = 0; inc < n ; inc++)
 {
-if ( i != n - 1)
-printf("%d ,", a[i]);
+if (inc != n - 1)
+printf("%d, ", a[inc]);
 else
-printf("%d", a[i]);
+printf("%d", a[inc]);
+
 }
 printf("\n");
 }
