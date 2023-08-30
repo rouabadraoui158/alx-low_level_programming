6230cb3ed7d89c58222d68bfc01535c0aec67452
roua <rouabadraoui158@gmail.com>
2023-08-30 06:35:37 -0700

    puts_recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif /* MAIN_H */
